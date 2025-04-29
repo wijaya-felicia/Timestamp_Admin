31a55ddacdfa94d2b864891cca688b51a489e40d
Lawrance Cancerlon <lawrance.cancerlon@binus.ac.id>
2025-04-29 16:12:45 +0700

    Removed admin delete button

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -6,12 +6,10 @@ import LoadingPage from "./LoadingPage";
 import { usePage, usePopup } from "../hooks/Context";
 import AddButton from "../components/AddButton";
 import { Overflow } from "../components/Overflow";
-import { ConfirmPopup } from "../components/Popup";
 
 const AdminPage: React.FC = () => {
     const { setPage } = usePage();
     setPage("");
-    const { showPopup, hidePopup } = usePopup();
     const { handleError } = errorHandler();
 
     const [ isFetching, setIsFetching ] = useState<boolean>(true);
@@ -32,23 +30,6 @@ const AdminPage: React.FC = () => {
         fetch();
     }, [isFetching]);
 
-    const destroy = (id: string) => {
-        showPopup(
-            <>
-                <ConfirmPopup message="Are you sure you want to delete this admin?" onConfirm={async () => {
-                    _user.delete(id)
-                        .then(() => {
-                            setIsFetching(true);
-                            hidePopup();
-                        }).catch(error => {
-                            handleError(error);
-                        })
-                    }} onCancel={hidePopup}
-                />
-            </>
-        )
-    }
-
     if(isFetching) {
         return <LoadingPage />;
     }
@@ -62,11 +43,6 @@ const AdminPage: React.FC = () => {
                             <div className="card bg-tertiary text-white h-100 w-100 text-decoration-none">
                                 <div className="card-body">
                                     <h4 className="card-title">{admin.name}</h4>
-                                    <div className="d-flex justify-content-end">
-                                        <a onClick={() => destroy(admin.id)} className="btn btn-danger">
-                                            Delete
-                                        </a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
