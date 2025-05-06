5472633db9f73226bf13b9fb86b7c550a76f64f0
Ananda Risyad <anandarisyad@proton.me>
2025-05-06 07:25:07 +0700

    Changes on log levels.
    order of the levels is reversed.
    booth logs for level above 2 is considered running.

--- src/pages/BoothDetailPage.tsx
+++ src/pages/BoothDetailPage.tsx
@@ -25,50 +25,59 @@ const BoothDetailPage: React.FC = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
-  const [ isInvalid, setIsInvalid] = useState<boolean>(false);
-  const [ isFetching, setIsFetching] = useState<boolean>(true);
+  const [isInvalid, setIsInvalid] = useState<boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(true);
 
-  const [ booth, setBooth ] = useState<Booth | undefined>(undefined);
-  const [ isFetchingBooth, setIsFetchingBooth ] = useState<boolean>(true);
-  const [ theme, setTheme ] = useState<Theme | undefined>(undefined);
-  const [ frames, setFrames ] = useState<Frame[]>([]);
-  const [ logs, setLogs ] = useState<BoothLog[]>([]);
-  const statusArray = ["Active", "Warning", "Error"];
+  const [booth, setBooth] = useState<Booth | undefined>(undefined);
+  const [isFetchingBooth, setIsFetchingBooth] = useState<boolean>(true);
+  const [theme, setTheme] = useState<Theme | undefined>(undefined);
+  const [frames, setFrames] = useState<Frame[]>([]);
+  const [logs, setLogs] = useState<BoothLog[]>([]);
+  const statusArray = ["Error", "Warning", "Active", "Active", "Active"];
 
   useEffect(() => {
     const fetch = async () => {
-      if(isFetchingBooth) {
-        _booth.get(id)
-          .then(response => {
-            if(response[0] && response[0].id === id) setBooth(response[0]);
+      if (isFetchingBooth) {
+        _booth
+          .get(id)
+          .then((response) => {
+            if (response[0] && response[0].id === id) setBooth(response[0]);
             else setIsInvalid(true);
-          }).catch(error => {
-              handleError(error);
-          }).finally(() => {
+          })
+          .catch((error) => {
+            handleError(error);
+          })
+          .finally(() => {
             setIsFetchingBooth(false);
-          })
-        _booth.getLogs(null, id)
-          .then(response => {
+          });
+        _booth
+          .getLogs(null, id)
+          .then((response) => {
             setLogs(response.reverse());
-          }).catch(error => {
-              handleError(error);
           })
-        _frame.get(null, null, null, null, id)
-          .then(response => {
-             setFrames(response);
-           }).catch(error => {
-               handleError(error);
-           })
-      }
-      else {
-        _theme.get(booth?.themeId)
-          .then(response => {
-            if(response[0].id === booth?.themeId) setTheme(response[0]);
-          }).catch(error => {
-              handleError(error);
-          }).finally(() => {
+          .catch((error) => {
+            handleError(error);
+          });
+        _frame
+          .get(null, null, null, null, id)
+          .then((response) => {
+            setFrames(response);
+          })
+          .catch((error) => {
+            handleError(error);
+          });
+      } else {
+        _theme
+          .get(booth?.themeId)
+          .then((response) => {
+            if (response[0].id === booth?.themeId) setTheme(response[0]);
+          })
+          .catch((error) => {
+            handleError(error);
+          })
+          .finally(() => {
             setIsFetching(false);
-          })
+          });
       }
     };
     fetch();
@@ -77,30 +86,34 @@ const BoothDetailPage: React.FC = () => {
   const destroy = () => {
     showPopup(
       <>
-        <ConfirmPopup message="Are you sure you want to delete this booth?" onConfirm={async () => {
+        <ConfirmPopup
+          message="Are you sure you want to delete this booth?"
+          onConfirm={async () => {
             if (id) {
-              await _booth.delete(id)
+              await _booth
+                .delete(id)
                 .then(() => {
                   hidePopup();
                   navigate("/booths", { replace: true });
                 })
-                .catch(error => {
+                .catch((error) => {
                   handleError(error);
                 });
-            } else hidePopup()
-          }} onCancel={() => {
+            } else hidePopup();
+          }}
+          onCancel={() => {
             hidePopup();
           }}
         />
-      </>
-    )
-  }
+      </>,
+    );
+  };
 
-  if(isFetching) {
+  if (isFetching) {
     return <LoadingPage />;
   }
 
-  if(isInvalid) {
+  if (isInvalid) {
     return <InvalidPage />;
   }
 
@@ -114,19 +127,21 @@ const BoothDetailPage: React.FC = () => {
                 <BackButton />
                 <h3 className="text-white mb-0 fw-bold">{booth?.name}</h3>
               </div>
-              <a className="btn btn-danger" onClick={destroy}>Delete Booth</a>
+              <a className="btn btn-danger" onClick={destroy}>
+                Delete Booth
+              </a>
             </div>
             <div className="d-flex align-items-center justify-content-start flex-wrap gap-3">
               <span
-                  className={"px-3 py-1 rounded-pill text-white ".concat(
-                      booth?.status == 0
-                      ? "bg-success"
-                      : booth?.status == 1
+                className={"px-3 py-1 rounded-pill text-white ".concat(
+                  booth?.status! >= 2
+                    ? "bg-success"
+                    : booth?.status == 1
                       ? "bg-warning"
-                      : "bg-danger"
-                  )}
-                  >
-                  {statusArray[booth?.status ?? 0]}
+                      : "bg-danger",
+                )}
+              >
+                {statusArray[booth?.status ?? 0]}
               </span>
               <span className="px-3 py-1 rounded-pill text-white text-nowrap bg-quaternary">
                 Server Key : {booth?.serverKey}
@@ -143,47 +158,56 @@ const BoothDetailPage: React.FC = () => {
           <hr />
           <div className="d-flex flex-column gap-3">
             <div className="d-flex gap-3 align-items-stretch">
-              <div className="flex-grow-1" style={{minWidth: "40%"}}>
+              <div className="flex-grow-1" style={{ minWidth: "40%" }}>
                 <div className="card bg-tertiary">
                   <div className="card-body">
-                    <h3 className="text-bold text-nowrap text-white">Theme : {theme?.name}</h3>
+                    <h3 className="text-bold text-nowrap text-white">
+                      Theme : {theme?.name}
+                    </h3>
                     <hr />
                     <div className="d-flex align-items-center">
-                      <img src={theme?.url} className="img-fluid rounded-3"/>
+                      <img src={theme?.url} className="img-fluid rounded-3" />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex-grow-1" style={{minWidth: "40%"}}>
+              <div className="flex-grow-1" style={{ minWidth: "40%" }}>
                 <div className="card bg-tertiary">
                   <div className="card-body">
-                      <h3 className="text-bold text-nowrap text-white">Booth Logs</h3>
-                      <hr />
-                      <Overflow height="calc(100vh - 444px)">
-                        <table className="table p-0">
-                          <tbody>
-                            {logs.map(log => (
+                    <h3 className="text-bold text-nowrap text-white">
+                      Booth Logs
+                    </h3>
+                    <hr />
+                    <Overflow height="calc(100vh - 444px)">
+                      <table className="table p-0">
+                        <tbody>
+                          {logs.map((log) => (
                             <tr key={log.id}>
                               <td className="bg-tertiary">
                                 <span
                                   className={"d-flex align-items-center justify-content-center py-1 rounded-pill text-white ".concat(
-                                  log?.level == 0
-                                    ? "bg-success"
-                                    : log?.level == 1
-                                    ? "bg-warning"
-                                    : "bg-danger"
-                                  )} style={{width: "80px"}}
+                                    log?.level >= 2
+                                      ? "bg-success"
+                                      : log?.level == 1
+                                        ? "bg-warning"
+                                        : "bg-danger",
+                                  )}
+                                  style={{ width: "80px" }}
                                 >
                                   {statusArray[log.level]}
                                 </span>
                               </td>
-                              <td className="text-white bg-tertiary">{log.message}</td>
-                              <td className="text-muted text-nowrap bg-tertiary">{unixToDate(log.timestamp)}</td>
+                              <td className="text-white bg-tertiary">
+                                {log.message}
+                              </td>
+                              <td className="text-muted text-nowrap bg-tertiary">
+                                {unixToDate(log.timestamp)}
+                              </td>
                             </tr>
-                            ))}
-                          </tbody>
-                        </table>
-                      </Overflow>
+                          ))}
+                        </tbody>
+                      </table>
+                    </Overflow>
                   </div>
                 </div>
               </div>
@@ -194,10 +218,19 @@ const BoothDetailPage: React.FC = () => {
                 <hr />
                 <OverflowX width="calc(100vw - 215px)">
                   <div className="d-flex w-100 align-items-stretch">
-                    {frames.map(frame => (
+                    {frames.map((frame) => (
                       <div key={frame.id} className="col-2 px-3">
-                        <img src={frame.url} className="w-100 object-fit-contain" style={{filter: "drop-shadow(0px 0px 5px #222)", height:"200px"}}/>
-                        <h5 className="text-center text-white mt-2">{frame.name}</h5>
+                        <img
+                          src={frame.url}
+                          className="w-100 object-fit-contain"
+                          style={{
+                            filter: "drop-shadow(0px 0px 5px #222)",
+                            height: "200px",
+                          }}
+                        />
+                        <h5 className="text-center text-white mt-2">
+                          {frame.name}
+                        </h5>
                       </div>
                     ))}
                   </div>
@@ -209,7 +242,7 @@ const BoothDetailPage: React.FC = () => {
       </Overflow>
       <EditButton />
     </>
-  )
-}
+  );
+};
 
 export default BoothDetailPage;
--- src/pages/BoothPage.tsx
+++ src/pages/BoothPage.tsx
@@ -8,71 +8,81 @@ import AddButton from "../components/AddButton";
 import { Overflow } from "../components/Overflow";
 
 const BoothPage: React.FC = () => {
-    const { setPage } = usePage();
-    setPage("Booths");
-    const { handleError } = errorHandler();
+  const { setPage } = usePage();
+  setPage("Booths");
+  const { handleError } = errorHandler();
 
-    const [ isFetching, setIsFetching ] = useState(true);
+  const [isFetching, setIsFetching] = useState(true);
 
-    const [ booths, setBooths ] = useState<Booth[]>([]);
-    const statusArray = ["Active", "Warning", "Error"];
+  const [booths, setBooths] = useState<Booth[]>([]);
+  const statusArray = ["Error", "Warning", "Active", "Active", "Active"];
 
-    useEffect(() => {
-        const fetch = async () => {
-            _booth.get()
-                .then(response => {
-                    setBooths(response);
-                }).catch(error => {
-                    handleError(error);
-                }).finally(() => {
-                    setIsFetching(false);
-                })
-        };
-        fetch();
-    }, []);
+  useEffect(() => {
+    const fetch = async () => {
+      _booth
+        .get()
+        .then((response) => {
+          setBooths(response);
+        })
+        .catch((error) => {
+          handleError(error);
+        })
+        .finally(() => {
+          setIsFetching(false);
+        });
+    };
+    fetch();
+  }, []);
 
-    if(isFetching) {
-        return <LoadingPage />;
-    }
+  if (isFetching) {
+    return <LoadingPage />;
+  }
 
-    return (
-        <>
-            <Overflow height="calc(100vh - 90px)">
-                <div className="d-flex flex-md-wrap">
-                    {booths.map((booth) => (
-                        <div key={booth.id} className="col-md-4 p-3">
-                        <a href={"/booths/".concat(booth.id)} className="card bg-tertiary text-white h-100 w-100 text-decoration-none">
-                            <div className="card-body">
-                                <h4 className="card-title">{booth.name}</h4>
-                                <p className="card-text text-truncate">{booth.description}</p>
-                                <div className="card-text d-flex align-items-center">
-                                    Status:
-                                    <span
-                                        className={"px-2 py-1 rounded-pill m-0 ms-2 ".concat(
-                                            booth.status == 0
-                                            ? "bg-success"
-                                            : booth.status == 1
-                                            ? "bg-warning"
-                                            : "bg-danger"
-                                        )}
-                                        >
-                                        {statusArray[booth.status]}
-                                    </span>
-                                </div>
-                                <div className="d-flex justify-content-end">
-                                    <a href={"/booths/".concat(booth.id)} className="btn btn-secondary">
-                                        Details
-                                    </a>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    ))}
+  return (
+    <>
+      <Overflow height="calc(100vh - 90px)">
+        <div className="d-flex flex-md-wrap">
+          {booths.map((booth) => (
+            <div key={booth.id} className="col-md-4 p-3">
+              <a
+                href={"/booths/".concat(booth.id)}
+                className="card bg-tertiary text-white h-100 w-100 text-decoration-none"
+              >
+                <div className="card-body">
+                  <h4 className="card-title">{booth.name}</h4>
+                  <p className="card-text text-truncate">{booth.description}</p>
+                  <div className="card-text d-flex align-items-center">
+                    Status:
+                    <span
+                      className={"px-2 py-1 rounded-pill m-0 ms-2 ".concat(
+                        booth.status >= 2
+                          ? "bg-success"
+                          : booth.status == 1
+                            ? "bg-warning"
+                            : "bg-danger",
+                      )}
+                    >
+                      {statusArray[booth.status]}
+                    </span>
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <a
+                      href={"/booths/".concat(booth.id)}
+                      className="btn btn-secondary"
+                    >
+                      Details
+                    </a>
+                  </div>
                 </div>
-            </Overflow>
-            <AddButton />
-        </>
-    )
-}
+              </a>
+            </div>
+          ))}
+        </div>
+      </Overflow>
+      <AddButton />
+    </>
+  );
+};
 
 export default BoothPage;
+
